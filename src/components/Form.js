14298e538770abcf24872a8f4651efc23b0147ee
Gianluca Andrisani <andrisanigianlucaux@gmail.com>
2024-07-03 09:32:14 +0100

    Added navigation for future prototypes

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../Form.css'; // Ensure correct import
+import Button from './Button';
 
 const Form = ({onSubmit}) => {
   const [formData, setFormData] = useState({
@@ -27,34 +28,28 @@ const Form = ({onSubmit}) => {
 
   return (
     <form className="form" onSubmit={handleSubmit}>
-      <div className='personal-details'>
-        <div className="form-group" style={{width: "100%"}}>
-          <label>Name</label>
-          <input type="text" name="name" className="form-input" value={formData.name} onChange={handleChange} />
-        </div>
-        <div className="form-group" style={{width: "100%"}}>
-          <label>Surname</label>
-          <input type="text" name="surname" className="form-input" value={formData.surname} onChange={handleChange} />
-        </div>
+    <div className='personal-details'>
+      <div className="form-group" style={{width: "100%"}}>
+        <input type="text" name="name" className="form-input" value={formData.name} onChange={handleChange} placeholder='Name'/>
       </div>
-      <div className="form-group">
-        <label>Age</label>
-        <input type="text" name="age" className="form-input" value={formData.age} onChange={handleChange} />
+      <div className="form-group" style={{width: "100%"}}>
+        <input type="text" name="surname" className="form-input" value={formData.surname} onChange={handleChange} placeholder='Surname' />
       </div>
-      <div className="form-group">
-        <label>Nationality</label>
-        <input type="text" name="nationality" className="form-input" value={formData.nationality} onChange={handleChange} />
-      </div>
-      <div className="form-group">
-        <label>Occupation</label>
-        <input type="text" name="occupation" className="form-input" value={formData.occupation} onChange={handleChange} />
-      </div>
-      <div className="form-group">
-        <label>Income</label>
-        <input type="text" name="income" className="form-input" value={formData.income} onChange={handleChange} />
-      </div>
-      <button type="submit" className='form-button'>Continue</button>
-    </form>
+    </div>
+    <div className="form-group">
+      <input type="text" name="age" className="form-input" value={formData.age} onChange={handleChange} placeholder='Age'/>
+    </div>
+    <div className="form-group">
+      <input type="text" name="nationality" className="form-input" value={formData.nationality} onChange={handleChange} placeholder='Nationality'/>
+    </div>
+    <div className="form-group">
+      <input type="text" name="occupation" className="form-input" value={formData.occupation} onChange={handleChange} placeholder='Occupation'/>
+    </div>
+    <div className="form-group">
+      <input type="text" name="income" className="form-input" value={formData.income} onChange={handleChange} placeholder='Occupation' />
+    </div>
+    <Button className='form' text="Continue" />
+  </form>
   );
 };
 
